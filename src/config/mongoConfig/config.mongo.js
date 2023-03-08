a2ce54ed5dcbf393d0e7f10a02936725851bab0c
catani-maximiliano <Catanimaximiliano@gmail.com>
2023-03-08 20:29:15 -0300

    version.1

--- src/config/mongoConfig/config.mongo.js
+++ src/config/mongoConfig/config.mongo.js
@@ -1,6 +1,21 @@
+
 const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')
+const session = require("express-session");
 
 const mongoConfig = () => {
+    app.use(session({
+        store: MongoStore.create({
+            mongoUrl:"mongodb+srv://admin:admin@cluster0.baldr.mongodb.net/sessions?retryWrites=true&w=majority",
+            mongoOptions:{useNewUrlParser: true},
+        }),
+        secret: "C0ntr4",
+        resave: false,
+        saveUninitialized: false
+    }))
+
+
+
     mongoose.set('strictQuery', false)
     mongoose.connect('mongodb+srv://admin:admin@cluster0.baldr.mongodb.net/?retryWrites=true&w=majority', error => {
     if (error) {
